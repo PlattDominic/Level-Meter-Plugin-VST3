b10f1b55271583cc2247f4d6182e98f72ac84c02
Dominic Martinez <villixmain@gmail.com>
2023-10-18 13:36:02 -0400

    Initial Commit(Project Complete)
    Initial commit with completed project files.

---
+++ Source/PluginEditor.h
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+#include "Component/HorizontalMeter.h"
+#include "Component/VerticalGradientMeter.h"
+#include "Component/VerticalDiscreteMeter.h"
+
+//==============================================================================
+/**
+*/
+class LevelMeterAudioProcessorEditor  : public juce::AudioProcessorEditor, public Timer
+{
+public:
+    LevelMeterAudioProcessorEditor (LevelMeterAudioProcessor&);
+    ~LevelMeterAudioProcessorEditor() override;
+
+    void timerCallback() override;
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    LevelMeterAudioProcessor& audioProcessor;
+
+    Gui::HorizontalMeter horizontalMeterL, horizontalMeterR;
+    Gui::VerticalGradientMeter verticalGradientMeterL, verticalGradientMeterR;
+    Gui::VerticalDiscreteMeter verticalDiscreteMeterL, verticalDiscreteMeterR;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LevelMeterAudioProcessorEditor)
+};
